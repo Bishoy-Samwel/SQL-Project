5f469b33d104de12f72ee406a83cabd4840e4f9e
Bishoy-Samwel <bishoy.samwuel@gmail.com>
2021-03-24 11:16:19 +0200

    --0 SELECT basics

---
+++ sql-zoo.sql
@@ -0,0 +1,21 @@
+--0 SELECT basics
+SELECT population FROM world   WHERE name = 'Germany'
+SELECT name, population FROM world   WHERE name IN ('Sweden', 'Norway', 'Denmark');
+SELECT name, area FROM world   WHERE area BETWEEN 200000 AND 250000
+--1 SELECT name
+
+--2 SELECT from World
+
+--3 SELECT from Nobel
+
+--4 SELECT within SELECT
+
+--5 SUM and COUNT
+
+--6 JOIN
+
+--7 More JOIN operations
+
+--8 Using Null
+
+--9 Self join
